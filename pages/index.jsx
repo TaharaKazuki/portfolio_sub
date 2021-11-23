a340e85123f774e80049af1dac148a69a176c0f8
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-11-23 22:33:12 +0900

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,12 @@ export default function Home() {
     <BaseLayout>
       <Container>
         <Button color="danger">Danger</Button>
+        <div className="parent-class sibling-class">
+          <h1>Hi there student</h1>
+          <h2>Hi there students</h2>
+          <div className="parent-class-title">Hello world</div>
+          <div className="parent-class-date">2000</div>
+        </div>
       </Container>
     </BaseLayout>
   )
