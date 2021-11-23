aeea90ac39295e7bfd9ee410166eb0566903c831
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-11-23 16:54:26 +0900

    update

--- pages/portfolios.jsx
+++ pages/portfolios.jsx
@@ -1,13 +1,29 @@
 import BaseLayout from '../components/layouts/BaseLayout'
+import axios from 'axios'
 
-const Portfolios = () => {
+const Portfolios = ({ posts }) => {
+  const renderPosts = () =>
+    posts.map((post) => <li key={post.id}>{post.title}</li>)
+
   return (
     <>
       <BaseLayout>
         <h1>I am portfolios page</h1>
+        <ul>{renderPosts()}</ul>
       </BaseLayout>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const { data } = await axios
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .catch((e) => {
+      console.info(e)
+    })
+  return {
+    props: { posts: data.slice(0, 10) },
+  }
 }
 
 export default Portfolios
