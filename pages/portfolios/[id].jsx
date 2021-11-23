3ea1d4c5c42d14e46edd30621b078317ba167e2c
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-11-23 21:44:30 +0900

    update

--- pages/portfolios/[id].jsx
+++ pages/portfolios/[id].jsx
@@ -1,17 +1,28 @@
 import BaseLayout from '../../components/layouts/BaseLayout'
 import { useRouter } from 'next/router'
+import axios from 'axios'
 
-const Portfolio = () => {
+const Portfolio = ({ portfolio }) => {
   const router = useRouter()
 
   return (
     <BaseLayout>
       <h1>I am portfolio Page</h1>
-      <h2>{router.query.id}</h2>
+      <h1>{portfolio.title}</h1>
+      <p>Body: {portfolio.body}</p>
+      <p>ID: {portfolio.id}</p>
     </BaseLayout>
   )
 }
 
-export const 
+export async function getServerSideProps({ query }) {
+  const { data } = await axios
+    .get(`https://jsonplaceholder.typicode.com/posts/${query.id}`)
+    .catch((e) => console.info(e))
+
+  return {
+    props: { portfolio: data },
+  }
+}
 
 export default Portfolio
