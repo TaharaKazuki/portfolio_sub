96abec1ae618a27e4d30ee4782db72a26ebf28ae
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-11-25 20:47:04 +0900

    update

--- pages/portfolios/[id].jsx
+++ pages/portfolios/[id].jsx
@@ -1,29 +1,35 @@
 import React from 'react'
 import BaseLayout from '@/components/layouts/BaseLayout'
 import BasePage from '@/components/BasePage'
-import axios from 'axios'
+import { useGetData } from '@/actions'
+import { useRouter } from 'next/router'
 
-const Portfolio = ({ portfolio }) => {
+const Portfolio = () => {
+  const router = useRouter()
+  const {
+    data: portfolio,
+    error,
+    loading,
+  } = useGetData(router.query.id ? `/api/v1/posts/${router.query.id}` : null)
+
   return (
     <BaseLayout>
       <BasePage>
-        <h1>I am portfolio Page</h1>
-        <h1>{portfolio.title}</h1>
-        <p>Body: {portfolio.body}</p>
-        <p>ID: {portfolio.id}</p>
+        {loading && <p>Loading Data...</p>}
+        {error.message && (
+          <div className="alert alert-danger">{error.message}</div>
+        )}
+        {portfolio && (
+          <>
+            <h1>I am portfolio Page</h1>
+            <h1>{portfolio.title}</h1>
+            <p>Body: {portfolio.body}</p>
+            <p>ID: {portfolio.id}</p>
+          </>
+        )}
       </BasePage>
     </BaseLayout>
   )
-}
-
-export async function getServerSideProps({ query }) {
-  const { data } = await axios
-    .get(`https://jsonplaceholder.typicode.com/posts/${query.id}`)
-    .catch((e) => console.info(e))
-
-  return {
-    props: { portfolio: data },
-  }
 }
 
 export default Portfolio
