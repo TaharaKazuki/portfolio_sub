8d11f4ede6aff2e24853177d9ca6258256e61217
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-11-26 00:06:24 +0900

    update

--- pages/api/v1/posts/[id].js
+++ pages/api/v1/posts/[id].js
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
 const handlerGetSelectId = async (req, res) => {
-  const { data } = await axios
-    .get(`https://jsonplaceholder.typicode.com/posts/${req.query.id}`)
-    .catch((e) => {
-      console.error(e)
-      res.status(e.status || 400).json({ message: 'Api error' })
-    })
-  res.status(200).json(data)
+  const { data } =
+    (await axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${req.query.id}`)
+      .catch((e) => {
+        res.status(e.status || 400).json({ message: 'Api error' })
+      })) || {}
+  if (data) {
+    res.status(200).json(data)
+  }
 }
 
 export default handlerGetSelectId
--- pages/api/v1/posts/index.js
+++ pages/api/v1/posts/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
 const handlerGet = async (req, res) => {
-  const { data } = await axios
-    .get('https://jsonplaceholder.typicode.com/posts')
-    .catch((e) => {
-      console.error(e)
-      res.status(e.status || 400).json({ message: 'Api Error' })
-    })
-  res.status(200).json(data.slice(0, 10))
+  const { data } =
+    (await axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .catch((e) => {
+        res.status(e.status || 400).json({ message: 'Api Error' })
+      })) || {}
+  if (data) {
+    res.status(200).json(data.slice(0, 10))
+  }
 }
 
 export default handlerGet
